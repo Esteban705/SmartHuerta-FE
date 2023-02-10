d0c6d56d2f7d7bd49bfb20624ea8de4e4be08635
Agustín Avellaneda <100479647+nitdraig@users.noreply.github.com>
2023-02-10 12:02:56 -0300

    User cards Ordenadas

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -4,96 +4,100 @@ import UserCards from "./UserCards";
 // import styles from '../../cssModules/AboutUs.module.css'
 
 const AboutUs = () => {
-    return (
-        <>
+    return (<>
+        <div style={
+            {
+                marginTop: "5rem",
+                width: "100%",
+                height: "500px",
+                backgroundColor: "#A5FCBC"
+
+
+            }
+        }>
             <div style={
-                {
-                    marginTop: "5rem",
-                    width: "100%",
-                    height: "500px",
-                    backgroundColor: "#A5FCBC"
-
-
-                }
+                {}
             }>
-                <div style={
-                    {}
-                }>
-                    <h1 style={
-                        {
-                            color: "#1F7345",
-                            fontSize: "30px",
-                            fontWeight: "bold",
-                            textAlign: "center",
-                            paddingTop: "2rem"
-                        }
-                    }>
-                        Usuarios de la Comunidad de SMART HUERTA
-                    </h1>
-                </div>
-                <div style={
+                <h1 style={
                     {
-                        marginTop: "15px",
-                        marginLeft: "100px",
-                        marginRight: "100px"
+                        color: "#1F7345",
+                        fontSize: "30px",
+                        fontWeight: "bold",
+                        textAlign: "center",
+                        paddingTop: "2rem"
                     }
                 }>
-                    <p style={
-                        {color: "#1F7345"}
-                    }>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-                                                                                                                                    dignissimos iure amet temporibus optio consequuntur non suscipit
-                                                                                                                                    placeat voluptate facere maiores dolorum corporis sequi quasi,
-                                                                                                                                    minima exercitationem, sint saepe officia.
-                    </p>
-                    <br/>
-                    <p style={
-                        {color: "#1F7345"}
-                    }>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-                                                                                                                                    dignissimos iure amet temporibus optio consequuntur non suscipit
-                                                                                                                                    placeat voluptate facere maiores dolorum corporis sequi quasi,
-                                                                                                                                    minima exercitationem, sint saepe officia.
-                    </p>
-                    <br/>
-                    <p style={
-                        {color: "#1F7345"}
-                    }>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-                                                                                                                                    dignissimos iure amet temporibus optio consequuntur non suscipit
-                                                                                                                                    placeat voluptate facere maiores dolorum corporis sequi quasi,
-                                                                                                                                    minima exercitationem, sint saepe officia.
-                    </p>
-                    <br/>
-                    <p style={
-                        {color: "#1F7345"}
-                    }>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-                                                                                                                                    dignissimos iure amet temporibus optio consequuntur non suscipit
-                                                                                                                                    placeat voluptate facere maiores dolorum corporis sequi quasi,
-                                                                                                                                    minima exercitationem, sint saepe officia.
-                    </p>
-                </div>
+                    Usuarios de la Comunidad de SMART HUERTA
+                </h1>
             </div>
-
             <div style={
                 {
-                    marginLeft: "2rem",
-                    marginTop: "5rem",
-                    display: "grid",
-                    gridTemplateRows: "1fr 1fr 1fr ",
-                    gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr 1fr ",
-                    gap: "0px",
-                    height: "100%"
+                    marginTop: "15px",
+                    marginLeft: "100px",
+                    marginRight: "100px"
                 }
             }>
-                {" "}
+                <p style={
+                    {color: "#1F7345"}
+                }>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                                                                                                                                                                                                                                                                                                                        dignissimos iure amet temporibus optio consequuntur non suscipit
+                                                                                                                                                                                                                                                                                                                        placeat voluptate facere maiores dolorum corporis sequi quasi,
+                                                                                                                                                                                                                                                                                                                        minima exercitationem, sint saepe officia.
+                </p>
+                <br/>
+                <p style={
+                    {color: "#1F7345"}
+                }>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                                                                                                                                                                                                                                                                                                                        dignissimos iure amet temporibus optio consequuntur non suscipit
+                                                                                                                                                                                                                                                                                                                        placeat voluptate facere maiores dolorum corporis sequi quasi,
+                                                                                                                                                                                                                                                                                                                        minima exercitationem, sint saepe officia.
+                </p>
+                <br/>
+                <p style={
+                    {color: "#1F7345"}
+                }>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                                                                                                                                                                                                                                                                                                                        dignissimos iure amet temporibus optio consequuntur non suscipit
+                                                                                                                                                                                                                                                                                                                        placeat voluptate facere maiores dolorum corporis sequi quasi,
+                                                                                                                                                                                                                                                                                                                        minima exercitationem, sint saepe officia.
+                </p>
+                <br/>
+                <p style={
+                    {color: "#1F7345"}
+                }>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                                                                                                                                                                                                                                                                                                                        dignissimos iure amet temporibus optio consequuntur non suscipit
+                                                                                                                                                                                                                                                                                                                        placeat voluptate facere maiores dolorum corporis sequi quasi,
+                                                                                                                                                                                                                                                                                                                        minima exercitationem, sint saepe officia.
+                </p>
+            </div>
+        </div>
+
+        <div style={
+            {
+                marginLeft: "2rem",
+                marginTop: "5rem",
+                display: "grid",
+                gridTemplateRows: "1fr 1fr 1fr ",
+                gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr 1fr ",
+                gap: "0px",
+                height: "100%"
+            }
+        }>
+            <div style={
+                {
+                    display: "flex",
+                    marginLeft: "10rem"
+                }
+            }>
                 <UserCards/>
                 <UserCards/>
-                <UserCards/>{" "} </div>
-            <Footer/>
-        </>
-    );
+                <UserCards/></div>
+        </div>
+        <Footer/>
+    </>);
 };
 
 export default AboutUs;
--- src/components/About/UserCards.jsx
+++ src/components/About/UserCards.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 const UserCards = () => {
-  return (
-    <>
-    <div style={{ width:"250px",
-                  height:"300px",
-                  backgroundColor:"blue"
-                }}  ></div>
-    </>
-  )
+    return (<>
+        <div style={
+            {
+                width: "250px",
+                height: "300px",
+                backgroundColor: "blue",
+                margin: "30px"
+
+
+            }
+        }></div>
+    </>)
 }
 
 export default UserCards
